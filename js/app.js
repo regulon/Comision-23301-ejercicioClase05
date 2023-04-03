adcd05d9d3902edad1fa0be112ecb29ea811f552
regulon <reguloluna@gmail.com>
2023-04-03 13:39:12 -0300

    Código limpio

--- js/app.js
+++ js/app.js
@@ -1,48 +1,3 @@
-/* class CreateInput{
-    constructor (parentId, type, classBoostrap, id, placeholder, name){
-        this.parentId = parentId;
-        this.type = type;
-        this.classBoostrap = classBoostrap;
-        this.id = id;
-        this.placeholder = placeholder;
-        this.name = name;
-    }
-    render() {
-        let inputForm = document.getElementById(this.parentId);
-        const myInputForm = `${inputForm.innerHTML}<input type="${this.type}" class="${this.classBoostrap}" id="${this.id}" placeholder="${this.placeholder}" name="${this.name}">`;
-        inputForm.innerHTML = myInputForm;
-    }
-} */
-    /*renderInput(type, classBootstrap, id, placeholder, name) {
-         const inputForm = document.getElementById(this.parentId);
-        const inputElement = document.createElement('input');
-        inputElement.type = type;
-        inputElement.classList.add(classBootstrap);
-        inputElement.id = id;
-        inputElement.placeholder = placeholder;
-        inputElement.name = name;
-        const divElement = document.createElement('div');
-        divElement.classList.add('col-3');
-        divElement.appendChild(inputElement);
-        inputForm.appendChild(divElement);
-      }      
-}
-
-let primerInput = new CreateInput("capa", "password", "form-control w-25", "form", "Ingrese password", "form");
-primerInput.render();
-let primerInput1 = new CreateInput("capa", "text", "form-control w-25", "form1", "Ingrese texto", "form1");
-primerInput1.render();
-primerInput.renderInput("password", "form-control", "form2", "Ingrese password", "form2");
-primerInput.renderInput("email", "form-control", "form3", "Ingrese email", "form3");
-
-function createInput(parentId, type, classBootstrap, id, placeholder, name) {
-    const inputForm = document.getElementById(parentId);
-    const myInputForm = `${inputForm.innerHTML}<input type="${type}" class="${classBootstrap}" id="${id}" placeholder="${placeholder}" name="${name}">`;
-    inputForm.innerHTML = myInputForm;
-}
-  
-createInput("capa", "email", "form-control w-25", "form4", "Ingrese email", "form4"); */
-
 class Elemento{
     constructor(parentId, type, classBoostrap, id, placeholder, texto, name){
         this.parentId = parentId;
@@ -59,7 +14,6 @@ class Elemento{
         elemento.innerHTML = myBoton;
     }
 };
-
 const elementoList = [
     {type: "button", classBoostrap: "btn btn-primary", value: "Click", id: "boton1", placeholder: "", name: "boton1"},
     {type: "submit", classBoostrap: "btn btn-danger", value: "Enviar", id: "boton2", placeholder: "", name: "boton2"},
@@ -78,24 +32,6 @@ const p = document.createElement('p');
 inputForm.appendChild(br);
 p.innerHTML = "Despues de usar la función filter"
 inputForm.appendChild(p);
-
-/* const filtrar = elementoList.filter(input => input.type === "text");
-
-const lista = document.createElement("ul");
-filtrar.forEach((elemento) => {
-  const li = document.createElement("li");
-  li.textContent = elemento.name;
-  lista.appendChild(li);
-});
-document.body.appendChild(lista); */
-
-/* const filtrar = elementoList.filter(input => input.type === "text");
-
-if (filtrar.length > 0) {
-  const elementoFiltrado = filtrar[0];
-  const instanciaFiltrado = new Elemento("capa", elementoFiltrado.type, elementoFiltrado.classBoostrap, elementoFiltrado.id, elementoFiltrado.placeholder, elementoFiltrado.value, elementoFiltrado.name);
-  instanciaFiltrado.render();
-} */
 
 const filtrar = elementoList.filter(input => input.type === "text");
 
